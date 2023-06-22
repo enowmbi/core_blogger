4af8ecaa5cba8bf8dbe9e96ac7f80e7d21b1c0c7
Enow Mbi <benowmbi@yahoo.com>
2023-06-22 15:23:14 +0100

    Add model specs for article.rb - validations and db columns

--- spec/models/core_blogger/article_spec.rb
+++ spec/models/core_blogger/article_spec.rb
@@ -2,5 +2,16 @@ require 'rails_helper'
 
 module CoreBlogger
   RSpec.describe Article, type: :model do
+    describe "database columns and indexes" do
+      it { is_expected.to have_db_column(:title).of_type(:string) }
+      it { is_expected.to have_db_column(:content).of_type(:text) }
+    end
+
+    describe "validations" do
+      it { is_expected.to validate_presence_of(:title) }
+      it { is_expected.to validate_uniqueness_of(:title).case_insensitive }
+      it { is_expected.to validate_presence_of(:content) }
+      it { is_expected.to have_rich_text(:content) }
+    end
   end
 end

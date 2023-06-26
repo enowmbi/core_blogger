d5212d623b190f7f797c1a261086aa26de15efe1
Enow Mbi <benowmbi@yahoo.com>
2023-06-26 17:17:41 +0100

    Setup hotwire and importmap

--- lib/core_blogger/engine.rb
+++ lib/core_blogger/engine.rb
@@ -1,11 +1,14 @@
 module CoreBlogger
   class Engine < ::Rails::Engine
+    require "hotwire-rails"
+    require "importmap-rails"
+
     isolate_namespace CoreBlogger
 
     config.generators do |g|
       g.test_framework :rspec
       g.fixture_replacement :factory_bot
-      g.factory_bot dir: 'spec/factories'
+      g.factory_bot dir: "spec/factories"
     end
   end
 end
